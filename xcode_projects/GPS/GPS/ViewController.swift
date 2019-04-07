c49a5df541a421976efc0a816d99ee2d10d35fe2
Admin <ryanchen2525@gmail.com>
2019-04-06 23:33:50 -0400

    mp3 player

--- xcode_projects/GPS/GPS/ViewController.swift
+++ xcode_projects/GPS/GPS/ViewController.swift
@@ -8,16 +8,29 @@
 
 import UIKit
 import CoreLocation
+import AVFoundation
 
 class ViewController: UIViewController {
 
     // current position
     let place = CLLocationCoordinate2D(latitude: 25, longitude: -25)
     
+    var player = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        do {
+            let path = Bundle.main.path(forResource: "Epic Sax Guy [Original] [HD]", ofType: "mp3")
+            try player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: path!) as URL)
+            // set session for playing backgroup
+            // also go to Capabilities to set up background mode
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSession.Category.playback)
+            player.play()
+        } catch {
+            
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
